6e321db9ae112749ed86130626afb2e12aee552b
Jan Rádl <radl.honza@gmail.com>
2020-11-30 14:04:03 +0100

    First commit

---
+++ components/Head/index.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+const HeadComponent = (props) => (
+  <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charSet="utf-8" />
+    <title>Hlava 22</title>
+    <meta name="description" content="Hlava 22" />
+    <meta property="og:title" content="Hlava 22" />
+    <meta property="og:description" content="Hlava 22" />
+    <meta name="pinterest" content="nopin" />
+
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://www.hlava22.band" />
+  </Head>
+);
+
+export default HeadComponent;
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import HeadComponent from "../components/Head";
+
+class Index extends Component {
+  render() {
+    return (
+      <>
+        <HeadComponent />
+        <h1>Hlava 22</h1>
+      </>
+    );
+  }
+}
+
+export default Index;
